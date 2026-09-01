114d7c8ef61726a8900420b78ada8c5d717ceb5d
DamianJS-ofc <damiandeveloper122@gmail.com>
2026-08-31 23:02:25 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,49 @@
+export default {
+    // === BOT INFO ===
+    name: 'NEXER BOT',
+    name1: 'NEXER BOT',
+    name2: 'NEXER BOT MD',
+    prefix: '.',
+    tipo: 'owner',
+    info: 'Iniciando sistema...',
+    version: '5.6.0',
+    navegador: ['Ubuntu', 'Chrome', '110.0.0.0'],
+    baileys: ['Edge', 'Chrome', '110.0.0.0'],
+    sessionName: 'session',
+
+    // === OWNER ===
+    botowner: '5492645746772',
+    creador: 'DamianJS-ofc',
+    owner: ['5492645746772', '5492645576493'],
+
+    // === STICKER / MEDIA ===
+    packname: 'NEXER BOT MD',
+    author: 'DamianJS-ofc',
+    img: 'https://raw.githubusercontent.com/JTxs00/uploads/main/1788207438786.jpeg',
+    imgMenu: 'https://raw.githubusercontent.com/JTxs00/uploads/main/1788207438786.jpeg',
+
+    // === CANAL - LIMPIO ===
+    channel: {
+      id: '120363425415754278@newsletter',
+      name: '𝙉𝙀𝙓𝙀𝙍 𝘽𝙊𝙏 𝙈𝘿',
+      link: 'https://whatsapp.com/channel/0029Vb7vqNDCsU9MnOn8UN0U'
+    },
+
+    // === ECONOMY ===
+    economy: {
+      initialCoins: 500,
+      initialExp: 0,
+      maxBet: 5000,
+      minBet: 5
+    },
+
+    mess: {
+      owner: '❌ Solo owner',
+      admin: '❌ Solo admins',
+      botAdmin: '❌ Necesito ser admin',
+      group: '❌ Solo en grupos',
+      wait: '⏳ Cargando...',
+      error: '❌ Error',
+      reg: '❌ No estás registrado, usa .reg nombre edad'
+    }
+}
